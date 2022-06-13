b9f61cbfc529b61d51c8d4dce3be80a8a0aef3f1
RyanHSL <105331995+Mrui22@users.noreply.github.com>
2022-06-12 22:32:18 -0400

    Add URL paths

--- Code/django/apps/urls.py
+++ Code/django/apps/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('user/<int:userId>', views.user_api)
+    path('user/<int:userId>', views.user_api),
+    path('movie/<int:movieId>', views.movie_api),
+    path('rating/<int:ratingId>', views.rating_api),
+    path('recommend/<int:userId>', views.recommend_moives)
 ]
