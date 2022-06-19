f52a9024596d45646d4309785f077d188d074e8f
Sujeet Kumar <sujeet@bu.edu>
2022-06-20 00:52:59 +0530

    Adding unit tests

--- Code/angular/hello-world/src/app/helpers/auth.guard.spec.ts
+++ Code/angular/hello-world/src/app/helpers/auth.guard.spec.ts
@@ -1,16 +1,21 @@
-// import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthenticationService } from '../services/authentication.service';
 
-// import { AuthGuard } from './auth.guard';
+import { AuthGuard } from './auth.guard';
 
-// describe('AuthGuard', () => {
-//   let guard: AuthGuard;
+describe('AuthGuard', () => {
+  let guard: AuthGuard;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//     guard = TestBed.inject(AuthGuard);
-//   });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+        imports: [RouterTestingModule, HttpClientModule]
+    });
+    guard = TestBed.inject(AuthGuard);
+  });
 
-//   it('should be created', () => {
-//     expect(guard).toBeTruthy();
-//   });
-// });
+  it('should be created', () => {
+    expect(guard).toBeTruthy();
+  });
+});
--- Code/angular/hello-world/src/app/services/authentication.service.spec.ts
+++ Code/angular/hello-world/src/app/services/authentication.service.spec.ts
@@ -1,16 +1,19 @@
-// import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
 
-// import { AuthenticationService } from './authentication.service';
+import { AuthenticationService } from './authentication.service';
 
-// describe('AuthenticationService', () => {
-//   let service: AuthenticationService;
+describe('AuthenticationService', () => {
+  let service: AuthenticationService;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//     service = TestBed.inject(AuthenticationService);
-//   });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+        imports: [HttpClientModule]
+    });
+    service = TestBed.inject(AuthenticationService);
+  });
 
-//   it('should be created', () => {
-//     expect(service).toBeTruthy();
-//   });
-// });
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
