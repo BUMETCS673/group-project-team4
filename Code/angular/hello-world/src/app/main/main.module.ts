b4baa921aed3c1afbb8d450fa7f23ac9fab2c89d
Sujeet Kumar <sujeet@bu.edu>
2022-06-19 15:53:24 +0530

    code cleanup

--- Code/angular/hello-world/src/app/main/main.module.ts
+++ Code/angular/hello-world/src/app/main/main.module.ts
@@ -3,9 +3,6 @@ import { RouterModule } from '@angular/router';
 import { mainRoutes } from './main-routing.module';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
-import { MovieListComponent } from './movie-list/movie-list.component';
-import { MovieDetailsComponent } from './movie-details/movie-details.component';
-import { MovieFormComponent } from './movie-form/movie-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { YearPipe } from '../pipes/year.pipe';
@@ -14,9 +11,6 @@ import { TitlePipe } from '../pipes/title.pipe';
 @NgModule({
     declarations: [
         MainComponent,
-        MovieListComponent,
-        MovieDetailsComponent,
-        MovieFormComponent,
         TitlePipe,
         YearPipe
     ],
